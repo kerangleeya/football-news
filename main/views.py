7db979a518fe372f2daf657b15c4fb6f712d9ce8
kerangleeya <shelia.vellicita13@gmail.com>
2025-09-10 15:23:33 +0700

    tutorial 2

--- main/views.py
+++ main/views.py
@@ -1,13 +1,17 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
 
 def show_main(request):
+    news_list = News.objects.all()
+
     context = {
         'npm' : '2406453606',
         'name': 'Shelia Vellicita',
-        'class': 'PBP KI',
-        'news_list': 'news_list'
+        'class': 'PBD KI',
+        'news_list': news_list
     }
 
     return render(request, "main.html", context)
@@ -31,3 +35,29 @@ def show_news(request, id):
     }
 
     return render(request, "news_detail.html", context)
+
+def show_xml(request):
+    news_list = News.objects.all()
+    xml_data = serializers.serialize("xml", news_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    news_list = News.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, id):
+    try:
+        news_item = News.objects.filter(pk=id)
+        xml_data = serializers.serialize("xml", news_item)
+        return HttpResponse(xml_data, content_type="application/xml")
+    except News.DoesNotExist:
+        return HttpResponse(status=404)
+   
+def show_json_by_id(request, id):
+    try:
+        news_item = News.objects.get(pk=id)
+        json_data = serializers.serialize("json", [news_item])
+        return HttpResponse(json_data, content_type="application/json")
+    except News.DoesNotExist:
+        return HttpResponse(status=404)
