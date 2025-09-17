d90ab2fcc098bcc3173085ddf96a553e7d319291
kerangleeya <shelia.vellicita13@gmail.com>
2025-09-17 14:18:16 +0700

    Tutorial 3

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shelia-vellicita-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://shelia-vellicita-footballnews.pbp.cs.ui.ac.id"
+    "https://shelia-vellicita-footballnews.pbp.cs.ui.ac.id",
 ]
 
 # Application definition
